d75bed86929e84191a3c180e0a35425625e6d420
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-08-09 19:01:58 -0600

    Day 10 – Structs, computed properties, and property observers

--- Contents.swift
+++ Contents.swift
@@ -675,3 +675,100 @@ let resultado = luckyNumbers
 resultado.forEach { (line: String) in
     print(line)
 }
+
+
+/// Día 10 : estructuras, propiedades calculadas y observadores de propiedades
+///
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+
+
+//struct Employee {
+//    let name: String
+//    var vacationRemaining: Int
+////    func takeVacation(days: Int) {
+//    mutating func takeVacation(days: Int) {
+//        if vacationRemaining > days {
+//            vacationRemaining -= days
+//            print("I'm going on vacation!")
+//            print("Days remaining: \(vacationRemaining)")
+//        } else {
+//            print("Oops! There aren't enough days remaining.")
+//        }
+//    }
+//}
+
+//
+//var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+//archer.takeVacation(days: 5)
+//print(archer.vacationRemaining)
+//
+//
+//var archer1 = Employee(name: "Sterling Archer", vacationRemaining: 14)
+//var archer2 = Employee.init(name: "Sterling Archer", vacationRemaining: 14)
+
+
+/// Cómo calcular los valores de las propiedades dinámicamente
+///
+
+
+//struct Employee {
+//    let name: String
+//    var vacationRemaining: Int
+//}
+//
+//var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+//archer.vacationRemaining -= 5
+//print(archer.vacationRemaining)
+//archer.vacationRemaining -= 3
+//print(archer.vacationRemaining)
+
+
+struct Employee {
+    let name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+
+//    var vacationRemaining: Int {
+//        vacationAllocated - vacationTaken
+//    }
+    var vacationRemaining: Int {
+        get {
+            vacationAllocated - vacationTaken
+        }
+
+        set {
+            vacationAllocated = vacationTaken + newValue
+        }
+    }
+}
+//
+//var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
+//archer.vacationTaken += 4
+//print(archer.vacationRemaining)
+//archer.vacationTaken += 4
+//print(archer.vacationRemaining)
+
+
+var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
+archer.vacationTaken += 4
+archer.vacationRemaining = 5
+print(archer.vacationAllocated)
