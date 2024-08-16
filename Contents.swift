84898dabf6b912f23d5a203e8dac6c4907b7cf3d
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-08-15 18:30:52 -0600

    Day 14 – Optionals, nil coalescing, and checkpoint 9

--- Contents.swift
+++ Contents.swift
@@ -1434,3 +1434,166 @@ myHouse.salesSummary()
 
 let myOffice = Office(numberOfRooms: 10, cost: 1000000, realtorName: "Karen Anastacia")
 myOffice.salesSummary()
+
+/// Día 14 : Opcionales, fusión nula y punto de control 9
+///
+
+let opposites = [
+    "Mario" : "Wario",
+    "Luigi":"Waluigi"
+]
+
+let peachOpposite = opposites["Peach"]
+
+if let marioOpposite = opposites["Mario"] {
+    print("Mario's opposite is \(marioOpposite)")
+}
+
+var username1: String? = nil
+
+if let unwrappedName = username1{
+    print("We got a user: \(unwrappedName)")
+} else {
+    print("The optional was empty.")
+}
+
+func square(number: Int) -> Int {
+    number * number
+}
+//
+//var number3: Int? = nil
+//print(square(number: number3))
+
+
+/// Cómo desenvolver opcionales con guardia
+///
+///
+///
+//func printSquare(of number: Int?) {
+//    guard let number = number else {
+//        print("Missing input")
+//        return
+//    }
+//
+//    print("\(number) x \(number) is \(number * number)")
+//}
+
+//var myVar: Int? = 3
+//
+//if let unwrapped = myVar {
+//    print("Run if myVar has a value inside")
+//}
+//
+//guard let unwrapped = myVar else {
+//    print("Run if myVar doesn't have a value inside")
+//}
+
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+
+        // 1: We *must* exit the function here
+        return
+    }
+
+    // 2: `number` is still available outside of `guard`
+    print("\(number) x \(number) is \(number * number)")
+}
+
+
+/// Cómo desenvolver opciones opcionales con fusión nula
+///
+///
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
+
+//let new = captains["Serenity"]
+
+//let new = captains["Serenity"] ?? "N/A"
+
+let new = captains["Serenity", default: "N/A"]
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
+
+
+struct Book1 {
+    let title: String
+    let author: String?
+}
+
+let book = Book1(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+
+let input = ""
+let number4 = Int(input) ?? 0
+print(number4)
+
+
+/// Cómo manejar múltiples opciones usando encadenamiento opcional
+
+
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+
+
+struct Book2 {
+    let title: String
+    let author: String?
+}
+
+var book2: Book2? = nil
+let author2 = book2?.author?.first?.uppercased() ?? "A"
+print(author)
+
+
+/// Cómo manejar fallas de funciones con opciones
+///
+/// 
+enum UserError: Error {
+case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+print("User: \(user)")
+}
+
+
+let user4 = (try? getUser(id: 23)) ?? "Anonymous"
+print(user4)
+
+
+///Punto de control 9
+///
+
+
+func randomNumber(from numbers: [Int]?) -> Int {
+    numbers?.randomElement() ?? Int.random(in: 1...100)
+}
+
+
+// Caso 1: Array no nulo con varios números
+let numbers1: [Int]? = [10, 20, 30, 40, 50]
+// Debería imprimir un número aleatorio del array
+print(randomNumber(from: numbers1))
+
+// Caso 2: Array vacío
+let numbers2: [Int]? = []
+// Debería imprimir un número aleatorio entre 1 y 100
+print(randomNumber(from: numbers2))
+
+// Caso 3: Array nulo
+let numbers3: [Int]? = nil
+// Debería imprimir un número aleatorio entre 1 y 100
+print(randomNumber(from: numbers3))
